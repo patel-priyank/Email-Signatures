95896a09a93f102622c6d73f5cd3f738d2bcc503
Priyank Patel <Priyank.Patel@ibm.com>
2024-02-20 02:57:17 +0530

    stop immediate repetition

--- script.js
+++ script.js
@@ -19,6 +19,11 @@ if (localStorage.getItem('data-theme') === 'dark') {
 (async () => {
   const signature = await (await fetch('./signatures.txt')).text();
   const signatures = signature.split('\n');
-  const random = Math.floor(Math.random() * signatures.length);
-  text.innerHTML = signatures[random];
+  let index = 0;
+  while (true) {
+    index = Math.floor(Math.random() * signatures.length);
+    if (index != localStorage.getItem('index')) break;
+  }
+  localStorage.setItem('index', index);
+  text.innerHTML = signatures[index];
 })();
