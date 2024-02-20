0ea28d01e2ba5d4e1245e92fc4cd88336040f748
Priyank Patel <Priyank.Patel@ibm.com>
2024-02-20 12:35:03 +0530

    remove theming

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
 const text = document.querySelector('.text');
-const theme = document.querySelector('.theme');
-
-theme.addEventListener('click', () => {
-  const attr = document.documentElement.getAttribute('data-theme');
-  if (attr) {
-    document.documentElement.removeAttribute('data-theme');
-    localStorage.removeItem('data-theme');
-  } else {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('data-theme', 'dark');
-  }
-});
-
-if (localStorage.getItem('data-theme') === 'dark') {
-  document.documentElement.setAttribute('data-theme', 'dark');
-}
 
 (async () => {
   const signature = await (await fetch('./signatures.txt')).text();
