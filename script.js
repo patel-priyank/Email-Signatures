d0f0661af2c919d9fe934ca1be489fae72084c60
Priyank Patel <Priyank.Patel@ibm.com>
2024-02-20 12:52:35 +0530

    added name to signature

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 const text = document.querySelector('.text');
+const name = document.querySelector('.name');
+const editName = document.querySelector('.edit-name');
+
+editName.addEventListener('click', () => {
+  let newName = prompt('Enter new name', name.innerHTML);
+  if (newName === '') {
+    newName = 'Elon Musk';
+  }
+  name.innerHTML = newName;
+  localStorage.setItem('name', newName);
+});
+
+(async () => {
+  let setName = localStorage.getItem('name');
+  if (setName) {
+    name.innerHTML = setName;
+  }
+})();
 
 (async () => {
   const signature = await (await fetch('./signatures.txt')).text();
